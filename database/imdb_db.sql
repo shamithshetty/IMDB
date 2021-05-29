f094df2b2c49a0aa2d57b6683af44749f7c38685
Shamith Shetty <shamithniz@gmail.com>
2021-05-29 16:30:51 +0530

    code modified

--- database/imdb_db.sql
+++ database/imdb_db.sql
@@ -5,6 +5,18 @@ CREATE DATABASE IF NOT EXISTS `imdb_db`;
 /*use the database*/
 
 use `imdb_db`;
+
+
+/*creating a table movie*/
+
+CREATE TABLE IF NOT EXISTS `movie` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`movie_number` VARCHAR(255) NOT NULL,
+	`movie_name` VARCHAR(250) NOT NULL,
+	`duration` VARCHAR(100) NULL,
+	`release_date` DATE  NULL,
+	CONSTRAINT pk_article_main_id PRIMARY KEY(`id`)
+	) AUTO_INCREMENT=1;
  
 /*creating a table article_download*/
 
@@ -25,17 +37,20 @@ CREATE TABLE IF NOT EXISTS `source_download` (
 CREATE TABLE IF NOT EXISTS `movie_type` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	`movie_type_name` VARCHAR(250) NOT NULL,
-	CONSTRAINT pk_movie_type_id PRIMARY KEY(`id`));
+	CONSTRAINT pk_movie_type_id PRIMARY KEY(`id`)
+	);
 
 CREATE TABLE IF NOT EXISTS `movie_genres` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	`genres_name` VARCHAR(250) NOT NULL,
 	CONSTRAINT pk_movie_type_id PRIMARY KEY(`id`));
 
+
 /*creating a table Movie Rating*/
 
 CREATE TABLE IF NOT EXISTS `movie_rating` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`movie_id`  INT UNSIGNED,
 	`rating` FLOAT(2),
 	`total_user` INT,
 	`one` INT,
@@ -48,26 +63,32 @@ CREATE TABLE IF NOT EXISTS `movie_rating` (
 	`eight` INT,
 	`nine` INT,
 	`ten` INT,
-	CONSTRAINT pk_movie_rating_id PRIMARY KEY(`id`));
+	CONSTRAINT pk_movie_rating_id PRIMARY KEY(`id`),
+	CONSTRAINT fk_movie_rating_id FOREIGN KEY(`movie_id`) REFERENCES movie(`id`) ON DELETE NO ACTION
+	);
 
 CREATE TABLE IF NOT EXISTS `movie_review` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`movie_id`  INT UNSIGNED,
 	`no_of_review` INT ,
 	`no_of_critic`  INT,
-	CONSTRAINT pk_movie_review_id PRIMARY KEY(`id`));
+	CONSTRAINT pk_movie_review_id PRIMARY KEY(`id`),
+	CONSTRAINT fk_movie_review_id FOREIGN KEY(`movie_id`) REFERENCES movie(`id`) ON DELETE NO ACTION);
 
 
 CREATE TABLE IF NOT EXISTS `technical_specs` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`runtime` VARCHAR(255) ,
+	`movie_id` INT UNSIGNED,
 	`sound_mix` VARCHAR(255) ,
 	`color` VARCHAR(255) ,
 	`aspect_ratio` VARCHAR(255) ,
-	CONSTRAINT pk_movie_review_id PRIMARY KEY(`id`));
+	CONSTRAINT pk_movie_review_id PRIMARY KEY(`id`),
+	CONSTRAINT fk_movie_technical_id FOREIGN KEY(`movie_id`) REFERENCES movie(`id`) ON DELETE NO ACTION);
 
 
 CREATE TABLE IF NOT EXISTS `movie_details`(
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`movie_id` INT UNSIGNED,
 	`movie_category`  VARCHAR(100),
 	`language` VARCHAR(100),
 	`country` VARCHAR(200) ,
@@ -76,46 +97,31 @@ CREATE TABLE IF NOT EXISTS `movie_details`(
 	`director_name` VARCHAR(100),
 	`production` VARCHAR(100),
 	`writer` VARCHAR(100),
-	CONSTRAINT pk_movie_details_id PRIMARY KEY(`id`));
+	`budget` VARCHAR(500),
+	`gross` VARCHAR(500),
+	CONSTRAINT pk_movie_details_id PRIMARY KEY(`id`),
+	CONSTRAINT fk_movie_moviedetails_id FOREIGN KEY(`movie_id`) REFERENCES movie(`id`) ON DELETE NO ACTION);
 
 CREATE TABLE IF NOT EXISTS `movie_story`(
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`movie_id` INT UNSIGNED,
 	`details` TEXT,
 	`story_line` TEXT,
-	CONSTRAINT pk_movie_story_id PRIMARY KEY(`id`));
+	CONSTRAINT pk_movie_story_id PRIMARY KEY(`id`),
+	CONSTRAINT fk_movie_story_id FOREIGN KEY(`movie_id`) REFERENCES movie(`id`) ON DELETE NO ACTION);
 
-/*creating a table movie*/
-
-CREATE TABLE IF NOT EXISTS `movie` (
-	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`movie_name` VARCHAR(250) NOT NULL,
-	`duration` VARCHAR(100) NOT NULL,
-	`release_date` DATE NOT NULL,
-	`rating_id` INT UNSIGNED NOT NULL,
-	`movie_details_id` INT UNSIGNED NOT NULL,
-	`review_id` INT UNSIGNED NOT NULL,
-	`movie_type_id` INT UNSIGNED,
-	`technical_id` INT UNSIGNED ,
-	`movie_download_id` INT UNSIGNED NOT NULL,
-	`movie_story_id` INT UNSIGNED ,
-	CONSTRAINT pk_article_main_id PRIMARY KEY(`id`),
-	CONSTRAINT fk_movie_type_id FOREIGN KEY(`movie_type_id`) REFERENCES movie_type(`id`) ON DELETE NO ACTION,
-	CONSTRAINT fk_rating_id FOREIGN KEY(`rating_id`) REFERENCES movie_rating(`id`) ON DELETE NO ACTION,
-	CONSTRAINT fk_review_id FOREIGN KEY(`review_id`) REFERENCES movie_review(`id`) ON DELETE NO ACTION,
-	CONSTRAINT fk_technical_id FOREIGN KEY(`technical_id`) REFERENCES technical_specs(`id`) ON DELETE NO ACTION,
-	CONSTRAINT fk_download_id FOREIGN KEY(`movie_download_id`) REFERENCES source_download(`id`) ON DELETE NO ACTION,
-	CONSTRAINT fk_details_id FOREIGN KEY(`movie_details_id`) REFERENCES movie_details(`id`) ON DELETE NO ACTION,
-	CONSTRAINT fk_story_id FOREIGN KEY(`movie_story_id`) REFERENCES movie_story(`id`) ON DELETE NO ACTION
-	) AUTO_INCREMENT=1;
 
 
 
 CREATE TABLE IF NOT EXISTS `actors`(
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`movie_id` INT UNSIGNED NOT NULL,
-	`name` VARCHAR(300) NOT NULL,
+	`actor_id` VARCHAR(100) default null,
+	`name` VARCHAR(300) default null,
 	`details` VARCHAR(500) ,
-	CONSTRAINT fk_movie_id FOREIGN KEY(`movie_id`) REFERENCES movie(`id`) ON DELETE NO ACTION
+	`img` VARCHAR(1000) default null,
+	UNIQUE KEY (movie_id, actor_id),
+	CONSTRAINT fk_movie_actors_id FOREIGN KEY(`movie_id`) REFERENCES movie(`id`) ON DELETE NO ACTION
 );
  
 CREATE TABLE IF NOT EXISTS `job_type` (
@@ -124,9 +130,11 @@ CREATE TABLE IF NOT EXISTS `job_type` (
 );
 
 
-INSERT INTO `job_type`(`id`,`job_type_name`) values (1,"Extractor");
+INSERT INTO `job_type`(`id`,`job_type_name`) values (1,"Date Range Extractor");
+INSERT INTO `job_type`(`id`,`job_type_name`) values (4,"Incremental Extractor");
 INSERT INTO `job_type`(`id`,`job_type_name`) values (2,"Parser");
 INSERT INTO `job_type`(`id`,`job_type_name`) values (3,"Movie_type_extractor");
+INSERT INTO `job_type`(`id`,`job_type_name`) values (5,"Movie_genres_extractor");
 
 
 CREATE TABLE IF NOT EXISTS `job` (
@@ -150,7 +158,9 @@ CREATE TABLE IF NOT EXISTS `job` (
 
 INSERT INTO `job`(`job_name`,`start_date`,`no_of_days`,`job_type`,`created_date`,`last_update`) values ("Date Range Extractor",'2016-01-01',0,1,now(),now());
 INSERT INTO `job`(`job_name`,`start_date`,`no_of_days`,`job_type`,`created_date`,`last_update`) values ("Parse",'2016-01-01',0,2,now(),now());
+INSERT INTO `job`(`job_name`,`start_date`,`no_of_days`,`job_type`,`created_date`,`last_update`) values ("Incremental Extractpr",'2016-01-01',0,4,now(),now());
 INSERT INTO `job`(`job_name`,`job_type`,`created_date`,`last_update`) values ("Movie type extractor ",3,now(),now());
+INSERT INTO `job`(`job_name`,`job_type`,`created_date`,`last_update`) values ("Movie genresextractor ",5,now(),now());
 
 
 CREATE TABLE IF NOT EXISTS `stats` (
@@ -189,6 +199,3 @@ CREATE TABLE IF NOT EXISTS `proxy` (
 	`username` VARCHAR(200),
 	`password` VARCHAR(200),
 	`created_date` DATETIME NOT NULL); 
-
-
-
